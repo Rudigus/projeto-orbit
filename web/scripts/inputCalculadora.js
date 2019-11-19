ab7fafcb2a5e46937a0b454ce4cc826d3dea5a1f
Rudigus <rodrigo.matos9@hotmail.com>
2019-11-18 21:36:05 -0300

    eita

--- web/scripts/inputCalculadora.js
+++ web/scripts/inputCalculadora.js
@@ -1,28 +1,25 @@
 var botoesAuxiliares = document.getElementById("botoesAuxiliares").children;
 var inputsCalculadora = document.getElementsByClassName("inputCalculadora");
 var ultimoInput = inputsCalculadora[0];
-console.log(botoesAuxiliares);
-console.log(botoesAuxiliares[0].alt);
-console.log(inputsCalculadora[1].value);
 for(i = 0; i < inputsCalculadora.length; i++)
 {
-  inputAtual = inputsCalculadora[i];
+  let inputAtual = inputsCalculadora[i];
   inputAtual.addEventListener("blur", function(){
     ultimoInput = event.target;
   })
 }
 for(i = 0; i < botoesAuxiliares.length; i++)
 {
-  botaoAtual = botoesAuxiliares[i];
+  console.log(botoesAuxiliares[i]);
+  let botaoAtual = botoesAuxiliares[i];
   botaoAtual.addEventListener("click", function(){
-    if(ultimoInput === inputsCalculadora[1])
+    if(ultimoInput === inputsCalculadora[0])
     {
-      console.log(botaoAtual);
-      inputsCalculadora[1].value += botaoAtual.alt;
+      inputsCalculadora[0].value += botaoAtual.children[0].alt;
     }
     else
     {
-      inputsCalculadora[0].value += botaoAtual.alt;
+      inputsCalculadora[1].value += botaoAtual.children[0].alt;
     }
   })
 }
