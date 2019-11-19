29b9d4b6b758c06151f09004be7903ca101da323
Alunos <Alunos>
2019-11-19 17:33:06 -0300

    ok

--- web/populador/populador.js
+++ web/populador/populador.js
@@ -3,16 +3,22 @@ var ld = document.getElementById("ld");
 var le = document.getElementById("le");
 var exibidor = document.getElementById("exibidor");
 ok.onclick = function printar() {
-  let equacao = {
-    polinomioEsquerdo: le.value,
-    polinomioDireito: ld.value
-  };
-  let cabecalho = {
-    elaborarResolucao: function(equacao){
-      let resultado = equacao.polinomioEsquerdo + equacao.polinomioDireito;
+ class equacao{
+   constructor(_polinomioEsquerda, _polinomioDireita){
+      this._polinomioEsquerda = le.value;
+      this._polinomioDireita = ld.value;
+    }
+  }
+  class cabecalho{
+    constructor(){}
+    elaborarResolucao(_lee,_ldd){
+      let resultado = ldd + lee;
       return resultado;
     }
-  };
-  var resultado = cabecalho.elaborarResolucao(equacao);
+  }
+  var lee = le.value;
+  var ldd = ld.value;
+  cabecalho1 = new cabecalho()
+  var resultado = cabecalho1.elaborarResolucao(lee,ldd);
   exibidor.innerHTML = "Resultado: " + resultado;
 };
