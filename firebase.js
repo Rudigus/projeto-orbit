9bf142259d211900d227c5b6e203ff5cb9c21405
José Luan Lima da Costa <54694573+jose-luan19@users.noreply.github.com>
2019-11-18 23:05:29 -0300

    Tutorial Database

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCokj3glVteUwc7ef3J5pfQ_LWSrYoYTMs",
+    authDomain: "first-6a712.firebaseapp.com",
+    databaseURL: "https://first-6a712.firebaseio.com",
+    projectId: "first-6a712",
+    storageBucket: "first-6a712.appspot.com",
+    messagingSenderId: "20367018934",
+    appId: "1:20367018934:web:188a28c460b57753584f17",
+    measurementId: "G-85C6PFV93D"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//firebase.analytics();
