4924bfcf69143c93edb2ee6ed188a270c4b427ac
Rudigus <rodrigo.matos9@hotmail.com>
2019-12-19 15:39:19 -0300

    ok

--- web/scripts/cabecalho.js
+++ web/scripts/cabecalho.js
@@ -4,7 +4,6 @@ function httpGetAsync(url, callback) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() {
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-            console.log(xmlHttp.responseText); // prints the ip address
             callback(xmlHttp.responseText);
         }
     }
@@ -22,8 +21,27 @@ function httpGetAsync(url, callback) {
 
 httpGetAsync('https://projeto-orbit.firebaseio.com/cabecalhos.json', function(texto){
   // Transforma o texto em um objeto
+  // var cabecalhos = texto;
   var cabecalhos = JSON.parse(texto);
-  console.log(cabecalhos);
+  var cabecalhosMatematica = cabecalhos.materia.matematica;
+  var cabecalhosFisica = cabecalhos.materia.fisica;
+  console.log(cabecalhosMatematica);
+  console.log(cabecalhosMatematica.length);
+  cabecalhosMatematica.forEach(function(topico){
+    console.log("2");
+    console.log(cabecalhosMatematica[i]);
+    Object.values(cabecalhosMatematica[i].titulo).forEach(function(tituloTopico)
+     {
+       console.log(tituloTopico);
+         // var item_lista = document.createElement("li");
+         // item_lista.innerHTML = textoExemplo;
+         // item_lista.style.listStyleType = "disc";
+         // lista_exemplos.appendChild(item_lista);
+     });
+  });
+});
+
+
   // var cabecalhoAtual = cabecalhos.cabecalho_01;
   //
   // Object.values(cabecalhoAtual.exemplos).forEach(function(textoExemplo)
@@ -33,7 +51,6 @@ httpGetAsync('https://projeto-orbit.firebaseio.com/cabecalhos.json', function(te
   //     item_lista.style.listStyleType = "disc";
   //     lista_exemplos.appendChild(item_lista);
   // });
-});
 
 /*
 httpGetAsync('https://projeto-orbit.firebaseio.com/cabecalhos.json', function(texto){
