ef222ddfd8f5ed533909846f2e570da6134f2d9f
Rudigus <rodrigo.matos9@hotmail.com>
2019-12-19 15:10:44 -0300

    ok

--- web/scripts/cabecalho.js
+++ web/scripts/cabecalho.js
@@ -19,9 +19,21 @@ function httpGetAsync(url, callback) {
 //     item_lista.style.listStyleType = "disc";
 //     lista_exemplos.appendChild(item_lista);
 // });
-httpGetAsync("https://projeto-orbit.firebaseio.com/cabecalhos/cabecalho_03/materia/fisica.json", function() {
-    console.log(this); // doesn't print the ip address
-})
+
+httpGetAsync('https://projeto-orbit.firebaseio.com/cabecalhos.json', function(texto){
+  // Transforma o texto em um objeto
+  var cabecalhos = JSON.parse(texto);
+  console.log(cabecalhos);
+  // var cabecalhoAtual = cabecalhos.cabecalho_01;
+  //
+  // Object.values(cabecalhoAtual.exemplos).forEach(function(textoExemplo)
+  // {
+  //     var item_lista = document.createElement("li");
+  //     item_lista.innerHTML = textoExemplo;
+  //     item_lista.style.listStyleType = "disc";
+  //     lista_exemplos.appendChild(item_lista);
+  // });
+});
 
 /*
 httpGetAsync('https://projeto-orbit.firebaseio.com/cabecalhos.json', function(texto){
